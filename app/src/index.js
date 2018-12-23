b848667b6cad0d2256ee7314b2ef1e442f4bfd0e
Elias Schablowski <eschablowski@me.com>
2018-12-22 19:02:10 -0800

    Updated and changed stuff

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import mediaMock from "uranium/matchMediaMock";
-
-global.matchMedia = mediaMock;
-// eslint-disable-next-line import/first
 import App from "./App";
 
 render(<App />, document.getElementById("root"));
