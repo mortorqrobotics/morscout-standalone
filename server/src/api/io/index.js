38fc47eb19d571de87d65fd06d33fab7f8c55560
Elias Schablowski <eschablowski@me.com>
2019-03-05 21:08:07 -0800

    fix

--- server/src/api/io/index.js
+++ server/src/api/io/index.js
@@ -5,50 +5,51 @@ import { loggedin } from "shared/types/Basic/LogIn";
 export default sock => {
   // console.log(socket);
   const socket = sock;
-  const send = () => (type, data) => socket.emit("action", {
-    type,
-    data
-  });
-  socket.on("getMatches", () => send(updatedMatches,
-      {
-        "1": {
-          time: "2017-12-17T17:00:00.000Z",
-          teams: {
-            blue: [
-              {
-                num: 1515
-              },
-              {
-                num: 1928
-              },
-              {
-                num: 1385
-              }
-            ],
-            red: [
-              {
-                num: 999
-              },
-              {
-                num: 666
-              },
-              {
-                num: 333
-              }
-            ]
-          },
-          progress: { current: 3, max: 6 }
-        }
+  const send = (type, data) =>
+    socket.emit("action", {
+      type,
+      data
+    });
+  socket.on("getMatches", () =>
+    send(updatedMatches, {
+      "1": {
+        time: "2017-12-17T17:00:00.000Z",
+        teams: {
+          blue: [
+            {
+              num: 1515
+            },
+            {
+              num: 1928
+            },
+            {
+              num: 1385
+            }
+          ],
+          red: [
+            {
+              num: 999
+            },
+            {
+              num: 666
+            },
+            {
+              num: 333
+            }
+          ]
+        },
+        progress: { current: 3, max: 6 }
       }
-    ));
-  socket.on("getTeams", () => send(updatedTeams,
-      {
-        "1515": {
-          matches: ["1"],
-          name: "MorTorq"
-        }
+    })
+  );
+  socket.on("getTeams", () =>
+    send(updatedTeams, {
+      "1515": {
+        matches: ["1"],
+        name: "MorTorq"
       }
-    ));
+    })
+  );
   socket.on("LOGIN", ({ username /* , password */ }) => {
     socket.user = {
       username,
