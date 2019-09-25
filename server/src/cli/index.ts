b89fa0dedd1f300d9dd5962f2ef568148b3b430c
Elias Schablowski <eschablowski@me.com>
2019-09-25 14:47:58 -0700

    Made everything docker compliant

--- server/src/cli/index.ts
+++ server/src/cli/index.ts
@@ -1,30 +1,13 @@
-import blessed from "blessed";
-import contrib from "blessed-contrib";
-import page1 from "./log";
-import page2 from "./dashboard";
-import page3 from "./control";
-import buttons from "./buttons";
+import install from "./install";
+import configEmitter from "config";
 
-export const screen = blessed.screen({
-  smartCSR: true,
-  fullUnicode: true,
-  dockBorders: true,
-  ignoreDockContrast: true,
-  title: "morscout"
-});
-screen.key(["escape", "C-c", "q"], (ch, key) => {
-  screen.destroy();
-  process.exit(0);
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (key: string)=>{
+  key = key.toLowerCase();
+  if(key == "i"){
+    install();
+  }
 });
 
-var carousel = new contrib.carousel([page1, page2, page3], {
-  screen: screen,
-  interval: 0, //how often to switch views (set 0 to never swicth automatically)
-  controlKeys: true //should right and left keyboard arrows control view rotation
-});
-carousel.start();
 
-buttons(screen);
-screen.key(["right", "left", "home", "end"], () => {
-  setImmediate(() => buttons(screen));
-});
+console.info("Press i to Setup the database");
